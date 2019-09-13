689966fa79a5403c677885b19b41dcfd3e862710
Jon Ward <projectNEXUSstudent6@gmail.com>
2019-09-13 11:43:15 -0700

    Animated files

---
+++ Animation/js/animeJS.js
@@ -0,0 +1,21 @@
+var textWrapper = document.querySelector('.AF');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.AF .letter',
+    translateY: [100,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 300 + 30 * i
+  })
+  /*.add({
+    targets: '.AF .letter',
+    translateY: [0,-100],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1200,
+    delay: (el, i) => 100 + 30 * i
+  });*/
