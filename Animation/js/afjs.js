4b43fba2de36fac16131a399f9652ff11a229d30
Jon Ward <projectNEXUSstudent6@gmail.com>
2019-09-16 06:48:46 -0700

    Sound update

--- Animation/js/afjs.js
+++ Animation/js/afjs.js
@@ -1,4 +1,6 @@
                 // Used to toggle the menu on small screens when clicking on the menu button
+var myJet;
+
 function myFunction() {
     var x = document.getElementById("navDemo");
     if (x.className.indexOf("w3-show") == -1) {
@@ -16,3 +18,23 @@ function openForm(){
 function closeForm(){
     document.getElementById("myForm").style.display="none";
 }
+  
+function mySound(){
+    myJet = new sound("./sounds/jet.mp3");
+    myJet.play();
+}
+
+function sound(src) {
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+      this.sound.play();
+    }
+    this.stop = function(){
+      this.sound.pause();
+    }
+  }
